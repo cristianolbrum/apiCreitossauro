aa0813b71487c88ad5e4d14c26d16c3c25fbd9db
Cristiano Brum <cristianolbrum@gmail.com>
2023-04-25 08:21:13 -0400

    primeiro commit

---
+++ src/test/java/com/creito/apiCreitossauro/ApiCreitossauroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creito.apiCreitossauro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCreitossauroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
